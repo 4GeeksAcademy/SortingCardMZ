225a1fb4adeba4e44f0c4c7b2d2465c8368c77cd
MarcelaZenteno93 <126695026+MarcelaZenteno93@users.noreply.github.com>
2024-01-12 21:57:11 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,152 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
+const cards = document.querySelectorAll(".card");
+const cardAmount = document.querySelector("form");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+cards.forEach(card => {
+  let topcorner = card.querySelector(".top-corner");
+  let bottomcorner = card.querySelector(".bottom-corner");
+  let cardnumber = card.querySelector(".card-number");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let selectedpinta = RandomPintaGenerator();
+
+  topcorner.classList.remove(topcorner.classList.item(1));
+  topcorner.classList.add(selectedpinta);
+  bottomcorner.classList.remove(bottomcorner.classList.item(1));
+  bottomcorner.classList.add(selectedpinta);
+  cardnumber.innerHTML = RandomNumberGenerator();
+});
+
+let RandomNumberGenerator = () => {
+  const possiblenumberpicks = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const numbertopick = Math.floor(Math.random() * possiblenumberpicks.length);
+  return possiblenumberpicks[numbertopick];
 };
+
+let RandomPintaGenerator = () => {
+  const pinta = ["heart", "diamond", "club", "spade"];
+  const pintatopick = Math.floor(Math.random() * pinta.length);
+  return pinta[pintatopick];
+};
+
+function createCard() {
+  const card = document.createElement("div");
+  card.className = "card";
+  const topCorner = document.createElement("span");
+  topCorner.className = "top-corner";
+  const bottomCorner = document.createElement("span");
+  bottomCorner.className = "bottom-corner";
+  const cardNumber = document.createElement("span");
+  cardNumber.className = "card-number";
+
+  let cardValue = RandomNumberGenerator();
+  let selectedPinta = RandomPintaGenerator();
+
+  topCorner.classList.add(selectedPinta);
+  bottomCorner.classList.add(selectedPinta);
+  cardNumber.innerHTML = cardValue;
+
+  card.dataset.value = getCardValue(cardValue);
+
+  card.appendChild(topCorner);
+  card.appendChild(cardNumber);
+  card.appendChild(bottomCorner);
+
+  return card;
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const x = parseInt(document.getElementById("amount").value, 10);
+
+  if (isNaN(x) || x < 1 || x > 10) {
+    alert("Please insert a number between 1 and 10");
+    return;
+  }
+
+  const cardContainer = document.querySelector(".container-random-cards");
+  cardContainer.innerHTML = "";
+  for (let i = 0; i < x; i++) {
+    cardContainer.appendChild(createCard());
+  }
+});
+
+function getCardValue(cardValue) {
+  const values = {
+    A: 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    J: 11,
+    Q: 12,
+    K: 13
+  };
+
+  return values[cardValue];
+}
+
+function selectionSort() {
+  const originalCards = document.querySelectorAll(
+    ".container-random-cards .card"
+  );
+  const cardsArr = Array.from(originalCards).map(card => ({
+    element: card.cloneNode(true),
+    value: parseInt(card.dataset.value)
+  }));
+
+  const sortedCardContainer = document.querySelector(".sorted-cards");
+  sortedCardContainer.innerHTML = "";
+
+  addCardsToContainer(originalCards, sortedCardContainer);
+
+  for (let i = 0; i < cardsArr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < cardsArr.length; j++) {
+      if (cardsArr[j].value < cardsArr[minIndex].value) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = cardsArr[i];
+      cardsArr[i] = cardsArr[minIndex];
+      cardsArr[minIndex] = temp;
+    }
+
+    addCardsToContainer(
+      cardsArr.map(cardInfo => cardInfo.element),
+      sortedCardContainer
+    );
+  }
+}
+
+function addCardsToContainer(cards, container) {
+  const row = document.createElement("div");
+  row.classList.add("row-cards");
+
+  cards.forEach(card => row.appendChild(card.cloneNode(true)));
+  container.appendChild(row);
+}
+
+const sortButton = document.querySelector("#sort");
+sortButton.addEventListener("click", selectionSort);
